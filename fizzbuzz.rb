4bd48d26562b6e5d9cb8cf66d4eba1aab92da1cf
Xavier C Barnes <xcbarnes@gmail.com>
2019-02-24 16:09:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 
 def fizzbuzz(int)
   if int % 15 == 0 
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif int % 5 == 0 
-    puts "Buzz"
+    return "Buzz"
   elsif int % 3 == 0 
     puts "Fizz"
   else 
